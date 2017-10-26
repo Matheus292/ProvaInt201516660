6e5dfb1973f89dadb1d580ac99e2516f7137cdd8
arqdsis <arqdsis@mocli10217.animaedu.intranet>
2017-10-26 19:29:16 -0200

    Initial commit

---
+++ app/src/main/java/br/usjt/provaint201516660/MainActivity.java
@@ -0,0 +1,14 @@
+package br.usjt.provaint201516660;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
